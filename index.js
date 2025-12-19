9e17eb4d8bfe7736b91dce6ab041f325ec0e4a91
developerakkoo <developerakshayjadhav@gmail.com>
2025-12-19 23:58:37 +0530

    new

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(morgan('dev'));
 
 // Serve static files from uploads directory
